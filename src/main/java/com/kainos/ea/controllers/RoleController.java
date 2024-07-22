94168e8f38153b59177119d3af51789e24e5d644
rackie-pascua <rackie.pascua@kainos.com>
2024-07-22 15:50:08 +0100

    Adding RoleService and RoleController

--- src/main/java/com/kainos/ea/controllers/RoleController.java
+++ src/main/java/com/kainos/ea/controllers/RoleController.java
@@ -1,4 +1,26 @@
 package com.kainos.ea.controllers;
 
+import com.kainos.ea.services.RoleService;
+import io.swagger.annotations.Api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+
+@Api("Job Role API")
+@Path("/api/job-roles")
 public class RoleController {
+    RoleService roleService;
+
+    public RoleController(final RoleService roleService) {
+        this.roleService = roleService;
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllJobRoles() throws SQLException {
+        return Response.ok().entity(roleService.getAllJobRoles()).build();
+    }
 }
--- src/main/java/com/kainos/ea/services/RoleService.java
+++ src/main/java/com/kainos/ea/services/RoleService.java
@@ -1,4 +1,20 @@
 package com.kainos.ea.services;
 
+import com.kainos.ea.daos.RoleDao;
+import com.kainos.ea.models.Role;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class RoleService {
+
+    RoleDao roleDao;
+
+    public RoleService(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    public List<Role> getAllJobRoles() throws SQLException {
+        return roleDao.getAllJobRoles();
+    }
 }
