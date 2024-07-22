5ad8182f951e422c6a65826bf44b209c3e0bb28a
ntotten-kainos <nathan.totten@kainos.com>
2024-07-22 15:55:43 +0100

    Password encoding with Argon2

--- src/main/java/com/kainos/ea/daos/AuthDao.java
+++ src/main/java/com/kainos/ea/daos/AuthDao.java
@@ -20,21 +20,28 @@ public class AuthDao {
                             + "AND `password` = ?;";
 
             Argon2PasswordEncoder passwordEncoder = new Argon2PasswordEncoder(16, 32, 1, 60000, 10);
-            String encodedPassword =
+            String encodedPassword = passwordEncoder.encode(loginRequest.getPassword());
 
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, loginRequest.getEmail());
-            statement.setString(2, loginRequest.getPassword());
+            statement.setString(2, encodedPassword);
 
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
                 return new User(
-                        resultSet.getString("username"),
+                        resultSet.getString("email"),
                         resultSet.getString("password")
                 );
             }
         }
         return null;
     }
+
+    /**
+     * When registering a user, be sure to create an Argon2PasswordEncoder instance to
+     *  encode the raw password BEFORE storing it in the DB.
+     * This will allow us to salt + hash the password securely and use argon2 to verify that
+     *  the passwords match without exposing the plaintext.
+     */
 }
