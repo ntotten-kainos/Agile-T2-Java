5a4a1f2a9aac19a28a75e110aa1a560d7aaa3b14
ntotten-kainos <nathan.totten@kainos.com>
2024-07-23 16:12:46 +0100

    Update V2__create_users_table.sql

--- src/main/resources/db/migration/V2__create_users_table.sql
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -3,6 +3,8 @@ CREATE TABLE `Users` (
     `password` VARCHAR(256) NOT NULL
 );
 
+-- When inserting data into `Users` table, it must first be run through an Argon2 Password encoder with params matching the encoder
+--  used by this application.
 INSERT INTO `Users`(`email`, `password`)
-VALUES  ('valid.admin@email.com', 'admin!Pa$$word123'),
-        ('user.name@gmail.com', '$up3rSecret%321');
+VALUES  ('valid.admin@email.com', '$argon2id$v=19$m=60000,t=10,p=1$osWewhkwSV5FkUs0cJnugg$QOA2RDScF9etxgLirmqYJGOdsZT+NkN6syS7WJoVSE0'),
+        ('user.name@gmail.com', '$argon2id$v=19$m=60000,t=10,p=1$H9sRUaEJb33/lAlfRe3vTA$VVBDjrhWez3z4CNAgDAFYSE/ukNSfh30/2Ssej8LY3E');
