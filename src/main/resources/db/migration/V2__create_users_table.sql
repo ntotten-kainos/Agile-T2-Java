a9e28453707c7db1c89d71fdec578bd2cb7d0479
ntotten-kainos <nathan.totten@kainos.com>
2024-07-24 15:13:29 +0100

    Update V2__create_users_table.sql
    Needed to add userRoleId

--- src/main/resources/db/migration/V2__create_users_table.sql
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS `Users` (
 	    `email` VARCHAR(64) PRIMARY KEY NOT NULL,
-        `password` VARCHAR(256) NOT NULL
+        `password` VARCHAR(256) NOT NULL,
+        `userRoleId` tinyint NOT NULL
 );
 
 -- When inserting data into `Users` table, it must first be run through an Argon2 Password encoder with params matching the encoder
 --  used by this application.
-INSERT INTO `Users`(`email`, `password`)
-VALUES  ('valid.admin@email.com', '$argon2id$v=19$m=60000,t=10,p=1$osWewhkwSV5FkUs0cJnugg$QOA2RDScF9etxgLirmqYJGOdsZT+NkN6syS7WJoVSE0'),
-        ('user.name@gmail.com', '$argon2id$v=19$m=60000,t=10,p=1$H9sRUaEJb33/lAlfRe3vTA$VVBDjrhWez3z4CNAgDAFYSE/ukNSfh30/2Ssej8LY3E');
+INSERT INTO `Users`(`email`, `password`, `userRoleId`)
+VALUES  ('valid.admin@email.com', '$argon2id$v=19$m=60000,t=10,p=1$osWewhkwSV5FkUs0cJnugg$QOA2RDScF9etxgLirmqYJGOdsZT+NkN6syS7WJoVSE0', 1),
+        ('user.name@gmail.com', '$argon2id$v=19$m=60000,t=10,p=1$H9sRUaEJb33/lAlfRe3vTA$VVBDjrhWez3z4CNAgDAFYSE/ukNSfh30/2Ssej8LY3E', 2);
