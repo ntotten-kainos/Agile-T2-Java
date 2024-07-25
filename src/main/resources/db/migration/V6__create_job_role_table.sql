ecf9bbf6057cdf81b27329911995c8ba27f9d2c7
rackie-pascua <rackie.pascua@kainos.com>
2024-07-25 13:10:54 +0100

    Fixes to SQL Scripts

--- src/main/resources/db/migration/V6__create_job_role_table.sql
+++ src/main/resources/db/migration/V6__create_job_role_table.sql
@@ -2,16 +2,15 @@ CREATE TABLE IF NOT EXISTS JobRoles (
     jobRoleId TINYINT NOT NULL AUTO_INCREMENT,
     roleName VARCHAR(64) NOT NULL,
     location VARCHAR(50) NOT NULL,
-    bandId TINYINT,
-    capabilityId TINYINT,
+    bandId TINYINT NOT NULL,
+    capabilityId TINYINT NOT NULL,
     closingDate DATETIME NOT NULL,
     status BOOLEAN NOT NULL,
     PRIMARY KEY (jobRoleId),
     FOREIGN KEY (bandId) REFERENCES Bands(bandId),
-    FOREIGN KEY (capabilityId) REFERENCES Capabilities(capabilityId)  -- Corrected this line
+    FOREIGN KEY (capabilityId) REFERENCES Capabilities(capabilityId)
 );
 
--- Insert data into JobRoles table
 INSERT INTO JobRoles (roleName, location, bandId, capabilityId, closingDate, status) VALUES
 ('Software Engineer', 'Belfast', 1, 7, '2024-12-31 23:59:59', true),
 ('Data Scientist', 'Derry', 2, 5, '2024-11-30 23:59:59', true),
