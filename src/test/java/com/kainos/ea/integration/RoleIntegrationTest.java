966560daaf3e1dfa1650a83493c2be43faef7503
rackie-pascua <rackie.pascua@kainos.com>
2024-07-26 09:45:17 +0100

    Attempt at the integration test that needs to be resolved

--- src/test/java/com/kainos/ea/integration/RoleIntegrationTest.java
+++ src/test/java/com/kainos/ea/integration/RoleIntegrationTest.java
@@ -10,7 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class RoleIntegrationTest {
@@ -27,5 +30,16 @@ public class RoleIntegrationTest {
 
         Assertions.assertFalse(response.isEmpty());
     }
+    @Test
+    void getAllJobRoles_shouldReturnServerError_whenFailedToRetrieveException() {
+        Client client = APP.client();
+
+        Response response = client.target("http://localhost:8080/api/job-roles")
+                .request()
+                .get();
+        assertEquals(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
+        assertEquals("An error occurred while retrieving job roles.", response.readEntity(String.class));
+    }
+
 }
 
