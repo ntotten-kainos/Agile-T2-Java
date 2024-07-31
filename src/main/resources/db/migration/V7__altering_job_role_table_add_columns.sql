e0689bc144f832bf099a482c821fa2ecffc89750
Ashna Abraham <ashna.abraham@kainos.com>
2024-07-31 12:28:25 +0100

    adding migration files

---
+++ src/main/resources/db/migration/V7__altering_job_role_table_add_columns.sql
@@ -0,0 +1,5 @@
+ alter table JobRoles
+ add column `description` VARCHAR(500) NOT NULL,
+ add column responsibilities VARCHAR(500) NOT NULL,
+ add column specification VARCHAR(300) NOT NULL,
+ add constraint specification_check CHECK (specification REGEXP 'https://');
