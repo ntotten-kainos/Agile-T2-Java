66a43848e8cb7fbeaf8b0fb9fa9d6e5a737f2cee
rackie-pascua <rackie.pascua@kainos.com>
2024-08-02 10:29:18 +0100

    Fixes

---
+++ src/main/resources/db/migration/V7__create_applicant_user_role.sql
@@ -0,0 +1,3 @@
+INSERT INTO `UserRoles`(`userRoleName`) VALUES ('APPLICANT');
+
+insert into `Users`(`email`, `password`, `userRoleId`) values ('regular.user@email.com', '$argon2id$v=19$m=60000,t=10,p=1$XqpmQ2+UdJjTw2rWbjY7vg$39Cse4trw5vilWP3FrPRrlUgt+uP0lSiMxl8CKd0xR0', 3);
