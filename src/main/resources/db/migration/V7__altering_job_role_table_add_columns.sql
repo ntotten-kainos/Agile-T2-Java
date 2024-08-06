cca8daf23ee7a0b7a933cc48f8d01a75100c4e9b
Ashna Abraham <ashna.abraham@kainos.com>
2024-08-06 13:39:44 +0100

    fixing sql syntax

--- src/main/resources/db/migration/V7__altering_job_role_table_add_columns.sql
+++ src/main/resources/db/migration/V7__altering_job_role_table_add_columns.sql
@@ -1,7 +1,6 @@
 TRUNCATE TABLE JobRoles;
  
-alter table JobRoles
- add column `description` VARCHAR(500) NOT NULL,
- add column responsibilities VARCHAR(500) NOT NULL,
- add column specification VARCHAR(300) NOT NULL,
- add constraint specification_check CHECK (specification REGEXP 'https://');
+ALTER TABLE JobRoles
+ ADD COLUMN `description` VARCHAR(500) NOT NULL,
+ ADD COLUMN responsibilities VARCHAR(500) NOT NULL,
+ ADD COLUMN specification VARCHAR(300) NOT NULL;
